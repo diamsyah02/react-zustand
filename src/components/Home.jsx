8afdc1679398cba252fcfa8f514780fe534ae58c
didadiamsyah02 <didadiamsyah02@gmail.com>
2022-12-27 08:40:30 +0700

    update store

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
   return (
     <>
-      <div className={`w-full h-screen flex justify-center items-center space-x-4 ${theme.isDark ? 'bg-red-500' : 'bg-blue-500'}`}>
+      <div className={`w-full h-screen flex justify-center items-center space-x-4 ${theme.isDark ? theme.color.dark : theme.color.light}`}>
         <button className="bg-white rounded py-3 p-6" onClick={theme.setTheme}>Switch Theme</button>
         <button className="bg-black text-white rounded py-3 p-6" onClick={auth.setIsLoggedIn}>Logout</button>
       </div>
--- src/stores/themeStore.js
+++ src/stores/themeStore.js
@@ -2,6 +2,10 @@ import create from "zustand";
 
 const useThemeStore = create((set) => ({
   isDark: false,
+  color: {
+    light : 'bg-gray-200',
+    dark: 'bg-gray-700',
+  },
   setTheme: () => set(state => ({ isDark: !state.isDark }))
 }))
 
